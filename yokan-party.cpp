6386f8a6e59ce561197c674fd611516675eb9df9
kelsher8 <52451760+kelsher8@users.noreply.github.com>
2022-08-17 20:39:57 +0900

    正答に寄せたコードに変更

--- yokan-party.cpp
+++ yokan-party.cpp
@@ -1,117 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <iostream>
+using namespace std;
 
+long long N, K, L;
+long long A[1 << 18];
 
-int calc_Score(int K,int N, int L,int* pattern,int* Array);
-int combination(int K, int N, int L,int* pattern, int* Array, int* ArrayLen, int decided,int*score);
-
-int main(){
-
-    int L=0;
-    int N=0;
-    int K=0;
-    
-
-    std::cin >> N >> L;    
-    std::cin >> K;
-
-    //test
-
-    int score = 0;
-
-    //()をつけることでnewを0クリアする
-    int* pattern = new int[N+2]();
-    int* Array = new int[N+2]();    
-    int* ArrayLen = new int[K+1]();
-
-    for(int i=1;i<N+1;i++){
-        std::cin >> Array[i] ;
+bool solve(long long M) {
+    long long cnt = 0, pre = 0;
+    for (int i = 1; i <= N; i++) {
+        if (A[i] - pre >= M && L - A[i] >= M) {
+            cnt += 1;
+            pre = A[i];
+        }
     }
-    Array[0] = 0;
-    Array[N + 1] = L;
-    pattern[0] = 1;
-    pattern[N+1] = 1;
+    if (cnt >= K) return true;
+    return false;
+}
 
-    score = combination(K,N,L,pattern,Array,ArrayLen,1,&score);
+int main() {
+    // Step #1. 入力
+    cin >> N >> L;
+    cin >> K;
+    for (int i = 1; i <= N; i++) {
+        cin >> A[i];
+    }
 
-    std::cout << score << std::endl;
+    // Step 答えで二分探索（めぐる式二分探索法）
+    // https://qiita.com/drken/items/97e37dd6143e33a64c8c
 
+    long long left = -1;
+    long long right = L + 1;
+    while (right - left > 1) {
+        long long mid = left + (right - left) / 2;
+        if (solve(mid) == false) right = mid;
+        else left = mid;
+    }
 
-    delete[] Array;
-    delete[] ArrayLen;
-    delete[] pattern;
-
+    cout << left << endl;
     return 0;
-}
-
-int combination(int K,int N,int L,int* pattern,int* Array,int* ArrayLen,int decided,int* score){
-    
-    int count = 0;
-    int tmp_score = 0;
-
-    int selected = 0;
-    for (int i = 1; i <= N ; i++) {
-        selected += pattern[i];
-    }
-
-
-    if (decided == N+1) {
-        if (selected==K) {
-            tmp_score = calc_Score(K, N, L,pattern, Array);
-            if (tmp_score > *score) {
-                *score = tmp_score;
-            }
-        }
-        return *score;
-    }
-
-
-    pattern[decided]=0;
-    combination(K,N,L,pattern,Array,ArrayLen,decided+1,score);
-
-    pattern[decided]=1;
-    combination(K,N,L,pattern,Array,ArrayLen,decided+1,score);
-
-    return *score;
-}
-
-int calc_Score(int K,int N,int L,int* pattern,int* Array){
-
-    int* ArrayLen = new int[K+1]();
-    int before=-1;
-    int after=-1;    
-    int start=0;
-    
-    for(int ksub=0;ksub<K+1;ksub++){
-        for(int i=start;i<N+2;i++){
-
-            if(pattern[i]==1){
-                if(before == -1){
-                        before = Array[i];
-                }else if(after==-1){
-                        after = Array[i];
-                        start=i;
-                }
-            }
-            if(before!=-1 && after!=-1){
-                ArrayLen[ksub]=after-before;
-                after = -1;
-                before = -1;
-                break;
-            }
-        }
-    }
-    
-    int score=L;
-
-    for(int i=0;i<K+1;i++){
-        if(ArrayLen[i]<score){
-            score = ArrayLen[i];
-        }
-    }
-
-    delete[] ArrayLen;
-    
-    return score;
 }
